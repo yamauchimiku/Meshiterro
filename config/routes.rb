01564a36956e52774d952aa58e0763b7838782f1
yamauchimiku <yamamochi7171@gmail.com>
2021-09-17 10:03:28 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
